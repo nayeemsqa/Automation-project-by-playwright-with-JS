d0add052eaae5c63091c10057ad5ed3b92fac5a0
Echologyx <mdnayeem.cse@gmail.com>
2024-08-20 11:09:52 +0600

    drop down and radio button selection

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -45,7 +45,34 @@ test("My first test case", async ({page})=>{
     //to extract all elements
     const allCardTitles = await cardTitles.allTextContents()
     console.log(allCardTitles);
+
 })
 
+test("dropdown and radio button selection", async ({page})=>{
+    const userName = page.locator("#username");
+    const password = page.locator("[type='password']");
+    const radioButton = page.locator(".radiotextsty");
+    const dropDown = page.locator("select.form-control")
+    const singIn = page.locator("#signInBtn")
+    const popupOkayButton = page.locator("#okayBtn")
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
 
+    await userName.type("rahulshettyacademy");
+    await password.type("learning");
+    await radioButton.last().click()
+    await popupOkayButton.click();
+    await page.pause()
+    await dropDown.selectOption("Student");
+})
+
+// test.only("Daals site popup remove", async ({page})=>{
+//     await page.goto("https://www.daals.co.uk/");
+//     console.log(await page.title());
+//     const homepageCollTitle = page.locator(".item1 h2");
+//     // await page.waitForLoadState('networkidle');
+//     console.log(await homepageCollTitle.allTextContents());
+//     await page.click('a[href="/collections/sofas"]');
+//     await page.waitForLoadState('networkidle');
+
+// })
 
